53d44e5bb958bba3f3a00f605a966751ff435a8e
Rohan Mondal <163380694+Rohanrolex@users.noreply.github.com>
2026-08-05 19:07:28 +0530

    Time: 2 ms (75.47%) | Memory: 18.9 MB (27.20%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> findOrder(int V, vector<vector<int>>& edges) {
+        
+          vector<vector<int>>adj(V);
+        
+        
+            for(auto it: edges){
+                int u = it[0];
+                int v = it[1];
+                adj[v].push_back(u);
+            }
+            
+            
+            queue<int>q;
+            vector<int>indegree(V,0);
+            
+            for(int i =0 ;i<V;i++){
+                for(auto it : adj[i]){
+                    indegree[it]++;
+                }
+            }
+            
+            
+            for(int i =0 ;i<V;i++){
+                if(indegree[i] ==0)q.push(i);
+            }
+            
+           vector<int>topo;
+            
+            while(!q.empty()){
+                int node = q.front();
+                q.pop();
+                topo.push_back(node);
+                
+                for(auto it : adj[node]){
+                    indegree[it]--;
+                    if(indegree[it] == 0)q.push(it);
+                }
+            }
+            
+            if(topo.size() != V)
+    return {};
+            
+            return topo;
+    }
+};
