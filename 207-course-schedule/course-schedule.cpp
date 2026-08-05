5845c9dca2c651afc8bfffe1ea8d4d4825179c75
Rohan Mondal <163380694+Rohanrolex@users.noreply.github.com>
2026-08-05 19:00:03 +0530

    Time: 4 ms (63.42%) | Memory: 20.1 MB (32.40%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool canFinish(int V, vector<vector<int>>& edges) {
+         
+        
+        vector<vector<int>>adj(V);
+        vector<int>indegree(V,0);
+        
+        for(auto it : edges){
+            int u = it[0];
+            int v= it[1];
+            
+            adj[u].push_back(v);
+        }
+        
+        for(int i =0 ;i<V;i++){
+            for(auto it : adj[i]){
+                indegree[it]++;
+            }
+       
+        }
+        
+        queue<int>q;
+        vector<int>topo;
+        
+        for(int i =0 ; i<V;i++){
+            if(indegree[i] ==0){
+                q.push(i);
+            }
+        }
+        
+        
+        while(!q.empty()){
+            
+          int node = q.front();
+          q.pop();
+          topo.push_back(node);
+          
+          for(auto it: adj[node]){
+              indegree[it]--;
+              
+              if(indegree[it] == 0) q.push(it);
+          }
+              
+        }
+
+        if(topo.size() == V) return true;
+        return false;
+    }
+};
