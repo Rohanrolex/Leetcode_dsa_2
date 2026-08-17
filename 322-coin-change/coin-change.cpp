e05cdb0418bf570132f68bfed32a5722b9359d75
Rohan Mondal <163380694+Rohanrolex@users.noreply.github.com>
2026-08-17 11:24:52 +0530

    Time: 62 ms (11.49%) | Memory: 49.8 MB (19.65%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
+int ans(int index ,vector<int>& coins, int amount, vector<vector<int>>& dp){
+ int n = coins.size();
+ int count =0;
+ if(amount == 0) return 0;
+ if(index == n){
+    
+    return 1e9;
+
+ }
+
+ if(dp[index][amount] != -1){
+    return dp[index][amount];
+ }
+
+int take =1e9;
+ if(coins[index] <= amount){
+     take = 1+ ans(index, coins , amount-coins[index], dp);
+ }
+
+ int nottake = ans(index+1 , coins , amount, dp);
+
+ count = min(take , nottake);
+
+ return dp[index][amount] =  count;
+
+
+}
     int coinChange(vector<int>& coins, int amount) {
-        if (amount < 1) return 0;
-        vector<int> dp(amount + 1, INT_MAX); // Initialize with INT_MAX
-        dp[0] = 0; // Base case
+         vector<vector<int>>dp(coins.size(), vector<int>(amount+1 , -1));
 
-        for (int i = 1; i <= amount; i++) {
-            for (int coin : coins) {
-                if (coin <= i && dp[i - coin] != INT_MAX) { // Check valid state
-                    dp[i] = min(dp[i], 1 + dp[i - coin]);
-                }
-            }
-        }
-
-        return dp[amount] == INT_MAX ? -1 : dp[amount]; // Return -1 if not possible
+     int ans1 = ans(0,coins , amount , dp);
+     if(ans1 == 1e9) return -1; //not possible
+     return ans1;
     }
 };
